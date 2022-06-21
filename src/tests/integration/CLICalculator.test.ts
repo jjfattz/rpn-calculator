ca528814008a07bd715a0b00de91c2d698a8664f
Admin <admin@Admins-MacBook-Pro.local>
2022-06-20 22:29:33 -0400

    Added CLICalculator.test.ts under integration tests

---
+++ src/tests/integration/CLICalculator.test.ts
@@ -0,0 +1,4 @@
+import { RPNCalculator } from "../../classes/RPNCalculator";
+import { CLICalculator } from "../../classes/CLICalculator";
+
+// ToDo: add an integration test for the CLI version of the calculator
--- src/tests/integration/basic.test.ts
+++
@@ -1,1 +0,0 @@
-// ToDo: add an integration test
