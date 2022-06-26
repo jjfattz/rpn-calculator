0306f7b84dab37bb2346e6f18f084327701b0a79
Admin <admin@Admins-MacBook-Pro.local>
2022-06-26 11:16:39 -0400

    Removed unnecessary CLICalculator class

--- src/tests/integration/CLICalculator.test.ts
+++
@@ -1,9 +0,0 @@
-import { RPNCalculator } from "../../classes/RPNCalculator";
-import { CLICalculator } from "../../classes/CLICalculator";
-
-// ToDo: add an integration test for the CLI version of the calculator
-describe("CLICalculator class", () => {
-    it("Sanity check", () => {
-        expect(true).toEqual(true);
-    })
-})
---
+++ src/tests/integration/integration.test.ts
@@ -0,0 +1,7 @@
+import { RPNCalculator } from "../../classes/RPNCalculator";
+
+describe("Integration test of RPNCalculator", () => {
+    it("Sanity check", () => {
+        expect(true).toEqual(true);
+    })
+})
